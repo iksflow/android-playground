3c484e1719179afe86f5107637b865f2cd5e3215
iksflow <theiksflow@gmail.com>
2023-09-07 13:01:25 +0900

    :sparkles: Change layout dependency from kotlin-extensions to view binding

---
+++ README.md
@@ -0,0 +1,10 @@
+# ADB(Android Debug Bridge) 설정
+ADB는 실제 기기와 연결해서 테스트 해볼 수 있는 환경을 제공한다.
+## 연결 방식
+### Wi-Fi
+개발자옵션 > 무선디버깅 > QR코드 인증
+### USB
+개발자옵션 > USB디버깅 설정
+# 참고
+* https://developer.android.com/studio/command-line/adb?hl=ko
+* https://developer.android.com/studio/run/device?hl=ko
--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidsample
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,53 +14,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidsample.ui.theme.AndroidSampleTheme
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-
+import com.example.androidsample.databinding.ActivityMainBinding
 // 실행되지는 않고 View Binding으로 마이그레이션 하라는 가이드가 나온다.
 // The 'kotlin-android-extensions' Gradle plugin is no longer supported. Please use this migration guide (https://goo.gle/kotlin-android-extensions-deprecation) to start working with View Binding (https://developer.android.com/topic/libraries/view-binding) and the 'kotlin-parcelize' plugin.
 class MainActivity : ComponentActivity() {
-    //    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            AndroidSampleTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
-//    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun convertCurrency(view: View) {
-        if (dollarText.text.isNotEmpty()) {
-            val dollarValue = dollarText.text.toString().toFloat()
+        if (binding.dollarText.text.isNotEmpty()) {
+            val dollarValue = binding.dollarText.text.toString().toFloat()
             val euroValue = dollarValue * 0.85f
-            textView.text = euroValue.toString()
+            binding.textView.text = euroValue.toString()
         } else {
-            textView.text = getString(R.string.no_value_string)
+            binding.textView.text = getString(R.string.no_value_string)
         }
     }
 }
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    AndroidSampleTheme {
-//        Greeting("Android")
-//    }
-//}
